490782ab206c3aa807007a872c3807262ca12338
Sunaljabili <you@example.com>
2020-08-07 03:02:24 +0400

    Carousel Completed with Slick Plugin

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+// Navbar Hover
     $(".dropdown").hover(function(){
       $(this).addClass("show");
       $(this).find(".dropdown-menu").addClass("show");
@@ -7,5 +7,18 @@ $(document).ready(function(){
         $(this).removeClass("show");
         $(this).find(".dropdown-menu").removeClass("show");
     })
+
+    if($(".one-time").length){
+      $('.one-time').slick({
+        dots: false,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow:'<i class="fas fa-long-arrow-alt-left"></i>',
+        nextArrow:'<i class="fas fa-long-arrow-alt-right"></i>'
+      });
+        
+    }
         
 });
