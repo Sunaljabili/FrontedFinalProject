47d8ea5d91fcc1b09272807914de9f6975c2be0f
Sunaljabili <you@example.com>
2020-08-09 02:19:41 +0400

    Accordion Page Completed with Responsive

--- js/script.js
+++ js/script.js
@@ -1,59 +1,88 @@
-$(document).ready(function(){
-// Navbar Hover
-    $(".dropdown").hover(function(){
-      $(this).addClass("show");
-      $(this).find(".dropdown-menu").addClass("show");
-    },function(){
-        $(this).removeClass("show");
-        $(this).find(".dropdown-menu").removeClass("show");
-    })
- 
-  // Slider Plugin
-  
+$(document).ready(function () {
 
-    // Slick  Plugin
-    if($(".one-time").length){
-      $('.one-time').slick({
-        dots: false,
-        infinite: true,
-        speed: 800,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        prevArrow:'<i class="fas fa-long-arrow-alt-left"></i>',
-        nextArrow:'<i class="fas fa-long-arrow-alt-right"></i>',
-      });
-        
-    }
-    // Slick Plugin
-    if($(".autoplay").length){
-      $('.autoplay').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        dots: false,
-        autoplaySpeed: 1000,
-        touchMove:true,
-        arrows:false,
-      });
-    }
-    $('.slider').slick({
+  // Index Page Start
+
+  // Navbar Hover
+  $(".dropdown").hover(function () {
+    $(this).addClass("show");
+    $(this).find(".dropdown-menu").addClass("show");
+  }, function () {
+    $(this).removeClass("show");
+    $(this).find(".dropdown-menu").removeClass("show");
+  })
+
+  // Slider Plugin
+
+
+  // Slick  Plugin
+  if ($(".one-time").length) {
+    $('.one-time').slick({
+      dots: false,
       infinite: true,
-      speed: 2000,
-      fade: true,
-      cssEase: 'linear',
+      speed: 800,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      prevArrow: '<i class="fas fa-long-arrow-alt-left"></i>',
+      nextArrow: '<i class="fas fa-long-arrow-alt-right"></i>',
+    });
+
+  }
+  // Slick Plugin
+  if ($(".autoplay").length) {
+    $('.autoplay').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 4500,
-      prevArrow: '<i class="fal fa-long-arrow-left"></i>',
-      nextArrow: '<i class="fal fa-long-arrow-right"></i>',
-      responsive: [{
-          breakpoint: 1040,
-          settings: {
-              infinite: true,
-              dots: true,
-              arrows: false,
-          }
-      }]
+      dots: false,
+      autoplaySpeed: 1000,
+      touchMove: true,
+      arrows: false,
+    });
+  }
+  // Sslick Plugin For Owl Carousel
+  $('.slider').slick({
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4500,
+    prevArrow: '<i class="fal fa-long-arrow-left"></i>',
+    nextArrow: '<i class="fal fa-long-arrow-right"></i>',
+    responsive: [{
+      breakpoint: 1040,
+      settings: {
+        infinite: true,
+        dots: true,
+        arrows: false,
+      }
+    }]
 
   });
-        
+
+// Index Page End 
+
+// Accordions Page Start
+
+  // Hover Accordion Version 1
+  $(document).on("click", ".card-header", function () {
+      $(this).next().slideDown("fast", function () {
+        $(this).addClass("active")
+      });
+      $(".active").slideUp("fast", function () {
+        $(this).removeClass("active");
+      })
+  })
+
+  // $(document).on("click", ".card-header", function () {
+  //   if ($(this).next()[0] != $("active")[0]) {
+  //     $(this).next().slideDown("fast", function () {
+  //       $(this).addClass("active")
+  //     });
+  //     $(".active").slideUp("fast", function () {
+  //       $(this).removeClass("active");
+  //     })
+  //   }
+  // })
+
 });
